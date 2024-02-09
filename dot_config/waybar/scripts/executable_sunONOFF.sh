e82fb3da55c572dff4be22526d2583702aecae17
Lorenzo <38138351+egoist000@users.noreply.github.com>
2024-02-10 00:31:19 +0100

    updated waybar config

--- dot_config/waybar/scripts/executable_sunONOFF.sh
+++ dot_config/waybar/scripts/executable_sunONOFF.sh
@@ -3,6 +3,11 @@
 if pgrep -x "wlsunset" > /dev/null
 then
     pkill wlsunset > /dev/null 2>&1
+    #echo 󰖨
+    #pkill -SIGRTMIN+8 waybar
 else
     wlsunset > /dev/null 2>&1 &
+    #echo 󰖔
+    #pkill -SIGRTMIN+8 waybar
 fi
+
