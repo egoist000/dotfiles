3c20c37aefabc6eb2d54d21c67a95c5f2a2b7a33
Lorenzo <38138351+egoist000@users.noreply.github.com>
2024-03-29 03:57:45 +0100

    update hyprland config and waybar

--- dot_config/waybar/scripts/executable_sunONOFF.sh
+++ dot_config/waybar/scripts/executable_sunONOFF.sh
@@ -6,7 +6,7 @@ then
     #echo 󰖨
     #pkill -SIGRTMIN+8 waybar
 else
-    wlsunset > /dev/null 2>&1 &
+    wlsunset -l 41.7 -L 12.3 > /dev/null 2>&1 &
     #echo 󰖔
     #pkill -SIGRTMIN+8 waybar
 fi
